2438cb344d6d2b6c64fed432f71680bb3ae95d0a
Amit Kumar <amit02543@yahoo.com>
2023-07-05 01:40:54 +0530

    delete event changes

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 
 import EventItem from '../components/EventItem';
 
@@ -27,4 +27,23 @@ export async function loader({ request, params }) {
     } 
 
     return response;
+};
+
+
+export async function action({ request, params }) {
+
+    const id = params.eventId;
+
+    const response = await fetch('http://localhost:8080/events/' + id, {
+        method: request.method
+    });
+
+
+
+    if(!response.ok) {
+        throw json({ message: 'Could not delet event.'}, { status: 500 });
+    } 
+
+
+    return redirect('/events');
 }
