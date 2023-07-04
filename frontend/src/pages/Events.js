3b74ef4b4406a2afac2e090d5d95eea042b0172f
Amit Kumar <amit02543@yahoo.com>
2023-07-05 00:46:21 +0530

    added error page

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData  } from 'react-router-dom';
+import { json, useLoaderData  } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
@@ -21,7 +21,8 @@ export async function loader() {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
-        //...
+        //throw new Response( JSON.stringify({ message: 'Could not fetch events.' }), { status: 500 });
+        throw json({ message: 'Could not fetch events.' }, { status: 500 });
     } else {
         const resData = await response.json();
 
